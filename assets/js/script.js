3d7029dffe813b38aa0242e5ffa31a8ec914e7e4
Jarl Grande <jarl.grande@gmail.com>
2022-04-24 22:38:56 -0700

    backround colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,32 @@
 var todayDate = moment().format('dddd, MMM Do YYYY - hh:mm:ss a');
 $("#currentDay").html(todayDate);
 
+// saveBtn click event listener
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+// backround color depending on current time
+$(".time-block").each(function() {
+    var currentTime = moment().hours();
+    console.log($(this)
+        .children()[0].textContent
+        .split(" ")[0]
+    );
+
+    var timeSlot = $(this)
+    .children()[0].textContent
+    .split(" ")[0];
+
+    console.log(currentTime)
+
+    if(currentTime > timeSlot) {
+        $(this).addClass("past");
+
+    } else if (currentTime == timeSlot) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+});
+
+
