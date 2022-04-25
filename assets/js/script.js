22ce9f012b096b74903f30a035ceb8c89f58143e
Jarl Grande <jarl.grande@gmail.com>
2022-04-24 22:01:13 -0700

    added timeblocks and fixed link to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // display today's date from jquery/moment.js tools
-var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayDate = moment().format('dddd, MMM Do YYYY - hh:mm:ss a');
 $("#currentDay").html(todayDate);
 
